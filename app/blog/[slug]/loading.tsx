893d299aac35aa1cf5921e3fbf454e0241fb5002
ktgcreative <ktgcreative@gmail.com>
2023-09-27 21:35:08 +1300

    added loading states

--- app/blog/[slug]/loading.tsx
+++ app/blog/[slug]/loading.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 const LoadingSkeleton: React.FC = () => {
     return (
         <section className='md:m-10 lg:m-16 p-4 animate-pulse'>
-            <div className="h-8 bg-gray-200 rounded dark:bg-gray-700 mb-4"></div> {/* Title */}
+            <div className="h-8 bg-gray-200 rounded dark:bg-gray-700 mb-4 w-1/2"></div> {/* Title */}
             
-            <div className="h-4 bg-gray-200 rounded dark:bg-gray-700 w-3/4 mb-6"></div> {/* Overview */}
-            <div className="h-4 bg-gray-200 rounded dark:bg-gray-700 w-2/3 mb-6"></div> {/* Explanation */}
+            <div className="h-4 bg-gray-200 rounded dark:bg-gray-700 w-3/4 mb-4"></div> {/* Overview label */}
+            <div className="h-4 bg-gray-200 rounded dark:bg-gray-700 w-3/4 mb-6"></div> {/* Overview content */}
+            
+            <div className="h-4 bg-gray-200 rounded dark:bg-gray-700 w-1/3 mb-4"></div> {/* Explanation label */}
+            <div className="h-4 bg-gray-200 rounded dark:bg-gray-700 w-2/3 mb-6"></div> {/* Explanation content */}
             
             <div className="code-container bg-gray-900 p-6 rounded-lg shadow-md overflow-auto ">
                 <pre className="text-white text-sm" style={{ backgroundColor: 'transparent' }}>
