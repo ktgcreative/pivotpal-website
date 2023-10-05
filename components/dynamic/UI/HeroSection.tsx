a182d65a25cf200f6c3029248ce0c5010fd050d5
ktgcreative <ktgcreative@gmail.com>
2023-10-06 04:34:54 +1300

    first commit

--- components/dynamic/UI/HeroSection.tsx
+++ components/dynamic/UI/HeroSection.tsx
@@ -34,7 +34,6 @@ const HeroSection: React.FC<HeroSectionProps> = ({
                     slug={''}
                     id={''}
                     codeContent={`
-
 !wget https://raw.githubusercontent.com/ktgcreative/pivotpal/main/pivotpal.py
 
 import pivotpal as pp
