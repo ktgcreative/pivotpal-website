85a996ed2f276fb09c430bcae6ffd17ce74cbd64
ktgcreative <ktgcreative@gmail.com>
2023-10-06 04:29:51 +1300

    first commit

--- components/dynamic/UI/HeroSection.tsx
+++ components/dynamic/UI/HeroSection.tsx
@@ -37,12 +37,7 @@ const HeroSection: React.FC<HeroSectionProps> = ({
                     !wget https://raw.githubusercontent.com/ktgcreative/pivotpal/main/pivotpal.py
                     pivotpal as pp`}
                 />
-                <DynamicCodeBox
-                    number={0}
-                    slug={''}
-                    id={''}
-                    codeContent={'# COMING SOON: pip install pivotpal as pp'}
-                />
+
                 <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">{description}</p>
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
                     <a href={primaryButtonLink} className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
