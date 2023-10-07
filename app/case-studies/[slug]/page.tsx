5ce92c1c63981dee43a98a054fdea55e06aadda2
ktgcreative <ktgcreative@gmail.com>
2023-10-08 02:18:40 +1300

    dynamic intro added

--- app/case-studies/[slug]/page.tsx
+++ app/case-studies/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import DataStream from "@/components/dynamic/coding/BlogSection";
-import DynamicIntroduction from "@/components/dynamic/coding/DynamicIntroduction";
+import DynamicCodeBox from "@/components/dynamic/coding/DynamicCodeBox";
+import Introduction from "@/components/dynamic/coding/Introduction";
 import Link from "next/link";
 
 interface CodeBoxData {
@@ -12,17 +13,12 @@ interface CodeBoxData {
     codeContent: string;
 }
 
-interface Step {
-    id: string;
-    title: string;
-}
-
 interface IntroductionData {
     number: number;
     slug: string;
     topic: string;
     overview: string;
-    steps: Step[];
+    steps: string[];
 }
 
 interface Props {
@@ -33,37 +29,37 @@ interface Props {
 
 
 export default async function BlogPostPage({ params }: Props) {
+
+
     const apiUrl = process.env.NODE_ENV === 'development'
         ? 'http://localhost:3000/api/case-blogs'
         : 'https://pivotpal.vercel.app/api/case-blogs';
 
-    const fetchData = async () => {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return await response.json();
-    };
+    const posts: CodeBoxData[] = await fetch(apiUrl)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return res.json();
+        })
+        .catch(error => {
+            console.error('Fetch error:', error);
+            return [];
+        });
 
-    let data;
-    try {
-        data = await fetchData();
-    } catch (error) {
-        console.error('Fetch error:', error);
-        return <div>Error fetching data!</div>;
-    }
 
-    const posts: CodeBoxData[] = data.codeBoxData;
-    const introduction: IntroductionData[] = data.introductionData; // You can use this later if needed
 
-    const currentIntroduction = introduction.find(intro => intro.slug === params.slug);
+
+    const response = await fetch(apiUrl);
+    const text = await response.text();
+
+
 
     const currentPost = posts.find(post => post.slug === params.slug);
+
     if (!currentPost) {
         return <div>Post not found!</div>;
     }
-
-    
 
     const nextPost = posts.find(post => post.number === currentPost.number + 1);
     const prevPost = posts.find(post => post.number === currentPost.number - 1);
@@ -72,13 +68,21 @@ export default async function BlogPostPage({ params }: Props) {
 
     return (
         <div className="p-4">
-
-
-              {/* <DynamicIntroduction
-                topic={currentIntroduction?.topic || "Default Topic"}
-                overview={currentIntroduction?.overview || "Default Overview"}
-                steps={currentIntroduction?.steps || []}
-            /> */}
+            <Introduction
+                topic="PivotPal: A Comprehensive Data Analysis Tool"
+                overview="PivotPal is a Python package designed to simplify common data analysis tasks, such as summarising large datasets, discovering patterns in distributions and finding missing values. It provides a set of functions that allow users to quickly generate insights from their data without the need for extensive coding. From understanding data distributions to identifying missing values, PivotPal offers a streamlined approach to data exploration."
+                steps={[
+                    "Helper Function: Provides a list of available functions in PivotPal and their descriptions.",
+                    "Distribution Function: Displays the distribution of values for a given column.",
+                    "Range Function: Shows the minimum and maximum values for each column in the dataset.",
+                    "Unique Function: Provides a count of unique values for each column.",
+                    "Summarise Function: Summarizes numeric columns with various statistics.",
+                    "Missing Function: Provides a summary of missing values for each column in the dataset.",
+                    "Zeros Function: Summarizes columns with zero values and their respective counts.",
+                    "Duplicates Function: Summarizes duplicate rows and columns in the dataset.",
+                    "Datatypes Function: Provides insights into the data types of columns in the dataset."
+                ]}
+            />
 
             {posts.filter(post => post.slug === params.slug).map(data => <DataStream key={data.id} {...data} />)}
             
