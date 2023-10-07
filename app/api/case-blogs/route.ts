9b460c81bf788a5eaa4811d0360bd5157b6772bb
ktgcreative <ktgcreative@gmail.com>
2023-10-08 02:10:13 +1300

    dynamic intro added

--- app/api/case-blogs/route.ts
+++ app/api/case-blogs/route.ts
@@ -1,29 +1,5 @@
 const DYNAMIC_CODEBOX_DATA = [
     {
-        number: 1,
-        slug: "sample-slug-1",
-        id: "sample-id-1",
-        title: "Sample Title 1",
-        overview: "This is a sample overview for post 1.",
-        explanation: "Here's a detailed explanation for post 1.",
-        codeContent: "pp.distribution(df)",
-        tableData: [
-            {
-                columnName: 'Feature',
-                data: ['FoodCourt', 'Spa', 'VRDeck', 'RoomService', 'ShoppingMall']
-            },
-            {
-                columnName: 'Total',
-                data: ['3,898,237.0', '2,647,791.0', '2,592,790.0', '1,912,541.0', '1,474,092.0']
-            },
-            {
-                columnName: 'Distribution (%)',
-                data: ['31.12', '21.14', '20.70', '15.27', '11.77']
-            }
-        ],
-        tableSummary: "Summary of the table data for post 1."
-    },
-    {
         "number": 2,
         "slug": "titanic",
         "id": "titanic-id-2",
