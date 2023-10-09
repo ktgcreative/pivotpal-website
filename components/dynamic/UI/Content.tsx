51d35171b8fff3359ad5352677c7019cab984ffa
ktgcreative <ktgcreative@gmail.com>
2023-10-10 01:50:29 +1300

    standardised dynamic routes

--- components/dynamic/UI/Content.tsx
+++ components/dynamic/UI/Content.tsx
@@ -28,7 +28,7 @@ const tableData = [
 const ContentSection: React.FC<ContentProps> = ({ title, paragraphs, images }) => {
   return (
     <section className="bg-white dark:bg-black">
-      <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <div className="gap-16 items-center  py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
         <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">{title}</h2>
           {paragraphs.map((paragraph, index) => (
