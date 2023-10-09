d224e1d33c9f790d1263fcfb7f113fa4b9c462c5
ktgcreative <ktgcreative@gmail.com>
2023-10-10 03:41:27 +1300

    fixed mobile nav

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -70,6 +70,7 @@ export default function Example() {
                   </a>
                 ))}
               </div>
+              
               <div className="space-y-2 py-6">
                 <p className='-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"'>Case Studies:</p>
                 {casestudies.map((item) => (
