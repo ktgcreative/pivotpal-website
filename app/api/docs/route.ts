0c1f2c48ad25d9f52668142ee1070093b6f228ba
ktgcreative <ktgcreative@gmail.com>
2023-10-09 21:54:37 +1300

    working??? - back to static intro, dynamic saved seperaty

--- app/api/docs/route.ts
+++ app/api/docs/route.ts
@@ -66,7 +66,26 @@ pp.distribution(df, 'AgeCategory')
             "codeContent": `
 pp.distribution(df, "Title")
             `
-        }
+        },
+        {
+            "number": 7,
+            "slug": "police",
+            "id": "police-id-1",
+            "title": "Exploring Missing Data in the Police Crime Dataset",
+            "overview": "A detailed analysis of the missing data within the Police Crime dataset.",
+            "explanation": "The Police Crime dataset provides insights into various crimes reported. In this exploration, we'll focus on identifying and understanding the missing data within this dataset.",
+            "codeContent": `
+        pp.missing(crime_data)
+        Column Name    Missing Count    Missing %
+        Context        19269992         100.0
+        Last outcome category   4356540 23.0
+        Crime ID       4036619          21.0
+        LSOA code      821598           4.0
+        LSOA name      821598           4.0
+        Longitude      322599           2.0
+        Latitude       322599           2.0
+            `
+        },
     ]
 ]
 
